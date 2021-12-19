10fe4476c5e7348ae21efefcdd29446f50a96c82
glaphire <9280486+glaphire@users.noreply.github.com>
2021-12-19 17:53:52 +0200

    L9. Created handler for failed authentication

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -4,11 +4,14 @@ namespace App\Security;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
@@ -17,13 +20,17 @@ use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
-    public function __construct(private UserRepository $userRepository)
+    public function __construct(
+        private UserRepository $userRepository,
+        private RouterInterface $router
+    )
     {
     }
 
     public function supports(Request $request): ?bool
     {
         return $request->getPathInfo() === '/login' && $request->isMethod('POST');
+
     }
 
     public function authenticate(Request $request): PassportInterface
@@ -49,12 +56,18 @@ class LoginFormAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        dd('success');
+        return new RedirectResponse(
+            $this->router->generate('app_homepage')
+        );
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        dd('failure');
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse(
+            $this->router->generate('app_login')
+        );
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
