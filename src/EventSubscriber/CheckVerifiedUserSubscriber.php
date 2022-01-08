bc3cf6869976cbeb7a728822ec3a4d9e94c9f210
glaphire <9280486+glaphire@users.noreply.github.com>
2022-01-09 00:21:26 +0200

    L41. Added 'LoginFailureEvent' handler with resend verification email logic

--- src/EventSubscriber/CheckVerifiedUserSubscriber.php
+++ src/EventSubscriber/CheckVerifiedUserSubscriber.php
@@ -3,18 +3,30 @@
 namespace App\EventSubscriber;
 
 use App\Entity\User;
+use App\Security\AccountNotVerifiedAuthenticationException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Http\Event\CheckPassportEvent;
+use Symfony\Component\Security\Http\Event\LoginFailureEvent;
 
 class CheckVerifiedUserSubscriber implements EventSubscriberInterface
 {
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
             CheckPassportEvent::class => ['onCheckPassport', -10],
+            LoginFailureEvent::class => 'onLoginFailure',
         ];
     }
 
@@ -33,9 +45,20 @@ class CheckVerifiedUserSubscriber implements EventSubscriberInterface
         }
 
         if (!$user->getIsVerified()) {
-            throw new CustomUserMessageAccountStatusException(
-                'Please verify your account before logging in'
-            );
+            throw new AccountNotVerifiedAuthenticationException();
         }
+    }
+
+    public function onLoginFailure(LoginFailureEvent $event)
+    {
+        if (!$event->getException() instanceof AccountNotVerifiedAuthenticationException) {
+            return;
+        }
+
+        $response = new RedirectResponse(
+            $this->router->generate('app_verify_resend_email')
+        );
+
+        $event->setResponse($response);
     }
 }
